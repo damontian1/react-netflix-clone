484c546842c3316ba6ffe1bcd9158bfd695d7316
Damon Tian <damontian@live.com>
2022-11-10 05:04:53 -0500

    fixed swiping to not use carousel plugin in mobile view

--- components/MobileMediaCarousel.js
+++ components/MobileMediaCarousel.js
@@ -18,76 +18,49 @@ export default function MediaCarousel(props) {
       })
   }
 
-  const handleTouchMove = () => {
-    document.querySelector("body").style.overflow = "hidden"
-  }
-  const handleTouchEnd = () => {
-    document.querySelector("body").style.overflow = "auto"
-  }
   return (
     <div className="mb-10">
       <span className="font-bold mb-1 inline-block capitalize text-2xl text-white">{category !== "top" ? category : `Top Picks for Damon`}</span>
-      <div onTouchMove={handleTouchMove} onTouchEnd={handleTouchEnd}>
-        <Carousel
-          partialVisible
-          infinite
-          renderButtonGroupOutside={false}
-          customRightArrow={<CarouselRightArrow />}
-          customLeftArrow={<CarouselLeftArrow />}
-          deviceType="mobile"
-          itemClass="px-0.5"
-          swipeable={true}
-          minimumTouchDrag="3"
-          responsive={{
-            mobile: {
-              breakpoint: { max: 640, min: 0 },
-              items: 3,
-              slidesToSlide: 3,
-              partialVisibilityGutter: 10
-            },
-          }}
-        >
-          {
-            state[category] && state[category].map((item, index) => {
-              if (category !== "myList") {
-                let itemTitle = item.original_title || item.original_name;
-                let itemTitleWithDashes = itemTitle.toLowerCase().split(" ").join("-")
-                return (
-                  <Link
-                    href={`/media/${itemTitleWithDashes}`}
-                    key={index}
-                    className="w-full h-full"
-                  >
-                    <a
-                      onClick={() => {
-                        fetchSimilarMedia(`https://api.themoviedb.org/3/movie/${item.id}/recommendations?api_key=631627e688738d84a1cae51aa035b23a`)
-                        state.setCurrentMedia(item);
-                        localStorage.setItem("currentMedia", JSON.stringify(item));
-                        state.setVisible(true);
-                      }}
-                      className="overflow-hidden inline-block rounded-sm relative h-full">
-                      <img src={`${process.env.assetPrefix}logo-small.png`} alt="" className="absolute mx-1 my-2 w-5" />
-                      <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="" className="w-full h-full object-cover" />
-                      <h2 className="absolute right-1 px-1 py-1 bottom-1 font-bold uppercase text-right text-base w-3/4 text-shadow-1 text-shadow-2 leading-none font-custom">{category === "popularTV" ? item.original_name : item.title}</h2>
-                      {category === "popularTV" && (<img src={`${process.env.assetPrefix}logo-new-episodes.png`} alt="" className="absolute bottom-2 h-5" />)}
-                    </a>
-                  </Link>
-                )
-              } else {
-                return (
-                  <div
-                    key={index}
-                    className="overflow-hidden inline-block rounded-sm relative h-full">
+      <div className="flex flex-row gap-x-1 overflow-x-auto scrollbar-hide">
+        {
+          state[category] && state[category].map((item, index) => {
+            if (category !== "myList") {
+              let itemTitle = item.original_title || item.original_name;
+              let itemTitleWithDashes = itemTitle.toLowerCase().split(" ").join("-")
+              return (
+                <Link
+                  href={`/media/${itemTitleWithDashes}`}
+                  key={index}
+                >
+                  <a
+                    onClick={() => {
+                      fetchSimilarMedia(`https://api.themoviedb.org/3/movie/${item.id}/recommendations?api_key=631627e688738d84a1cae51aa035b23a`)
+                      state.setCurrentMedia(item);
+                      localStorage.setItem("currentMedia", JSON.stringify(item));
+                      state.setVisible(true);
+                    }}
+                    className="overflow-hidden inline-block rounded-sm relative h-full w-44 flex-shrink-0">
                     <img src={`${process.env.assetPrefix}logo-small.png`} alt="" className="absolute mx-1 my-2 w-5" />
-                    <img src={`${item.poster_path}`} alt="" className="w-full h-full object-cover" />
+                    <img src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`} alt="" className="w-full h-full object-cover rounded-sm" />
                     <h2 className="absolute right-1 px-1 py-1 bottom-1 font-bold uppercase text-right text-base w-3/4 text-shadow-1 text-shadow-2 leading-none font-custom">{category === "popularTV" ? item.original_name : item.title}</h2>
                     {category === "popularTV" && (<img src={`${process.env.assetPrefix}logo-new-episodes.png`} alt="" className="absolute bottom-2 h-5" />)}
-                  </div>
-                )
-              }
-            })
-          }
-        </Carousel>
+                  </a>
+                </Link>
+              )
+            } else {
+              return (
+                <div
+                  key={index}
+                  className="overflow-hidden inline-block rounded-sm relative h-full w-44 flex-shrink-0">
+                  <img src={`${process.env.assetPrefix}logo-small.png`} alt="" className="absolute mx-1 my-2 w-5" />
+                  <img src={`${item.poster_path}`} alt="" className="w-full h-full object-cover" />
+                  <h2 className="absolute right-1 px-1 py-1 bottom-1 font-bold uppercase text-right text-base w-3/4 text-shadow-1 text-shadow-2 leading-none font-custom">{category === "popularTV" ? item.original_name : item.title}</h2>
+                  {category === "popularTV" && (<img src={`${process.env.assetPrefix}logo-new-episodes.png`} alt="" className="absolute bottom-2 h-5" />)}
+                </div>
+              )
+            }
+          })
+        }
       </div>
     </div >
   )
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="bg-black h-full w-full">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Netflix Clone | By Damon Tian</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
