22db7e78c5e92b5239385f3716cf5fff0057b6c6
Damon Tian <damontian@live.com>
2024-03-08 03:41:16 -0500

    added google verification meta tag

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className="bg-black h-full w-full">
       <Head>
+        <meta name="google-site-verification" content="oDOLRT8h2W1yC08Ux1fxQrMNAdxC1sYnZaJlld0nG1o" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Netflix Clone | By Damon Tian</title>
         <link rel="icon" href="/favicon.ico" />
